951bad5681b14dee251af3992e78cb6894aa8e85
Jose Polaco <polacojose@gmail.com>
2023-06-03 02:18:16 +1200

    Completed Day04 Part01

---
+++ 2015/day04/part1/src/main.rs
@@ -0,0 +1,18 @@
+use md5;
+use std::fs;
+
+fn main() {
+    let file_string = fs::read_to_string("input.txt").unwrap();
+    for line in file_string.lines() {
+        let secret = line.trim();
+
+        for answer in 0.. {
+            let digest = format!("{:x}", md5::compute(format!("{}{}", secret, answer)));
+
+            if digest.starts_with("00000") {
+                println!("{}", answer);
+                break;
+            };
+        }
+    }
+}
