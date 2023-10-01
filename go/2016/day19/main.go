7f9ed7eb2b95df048b2e75c3749d48c783f66b74
Jose Polaco <polacojose@gmail.com>
2023-10-01 17:17:04 +0100

    Completed day19

--- go/2016/day19/main.go
+++ go/2016/day19/main.go
@@ -5,18 +5,29 @@ import (
 	"fmt"
 )
 
-type elfCircle [3014387]bool
+const elfCircleSize = 3014387
+
+//const elfCircleSize = 5
+
+type Elf struct {
+	seat int
+}
 
 func main() {
-	getLastAliveInCircle()
+	//getLastAliveInCircle()
+	getLastAliveAcross()
 }
 
 func getLastAliveInCircle() {
-	elves := elfCircle{}
+	elves := make([]bool, elfCircleSize)
+	for i := 0; i < elfCircleSize; i++ {
+		elves[i] = true
+	}
+
 	killer := -1
 	for alive(elves) > 1 {
 		for i := 0; i < len(elves); i++ {
-			if elves[i] {
+			if !elves[i] {
 				continue
 			}
 
@@ -24,35 +35,49 @@ func getLastAliveInCircle() {
 				killer = i
 			} else {
 				killer = -1
-				elves[i] = true
+				elves[i] = false
 			}
 
 		}
 	}
 
-	lastIndex, _ := aliveIndex(elves)
+	lastIndex, _ := firstAliveIndex(elves)
 	fmt.Println(lastIndex + 1)
 }
 
-func aliveIndex(elves elfCircle) (int, error) {
+func getLastAliveAcross() {
+	elves := make([]Elf, elfCircleSize)
+	for i := 0; i < elfCircleSize; i++ {
+		elves[i] = Elf{i + 1}
+	}
 
+	for len(elves) > 1 {
+		midIndex := len(elves) / 2
+		elves = append(elves, elves[0])
+		elves = append(elves[1:midIndex], elves[midIndex+1:]...)
+		fmt.Println(len(elves))
+	}
+
+	fmt.Println(elves[0].seat)
+}
+
+func firstAliveIndex(elves []bool) (int, error) {
 	for i, e := range elves {
-		if !e {
+		if e {
 			return i, nil
 		}
 	}
 	return 0, errors.New("none alive")
 }
 
-func alive(elves elfCircle) int {
+func alive(elves []bool) int {
 	num := 0
 
 	for _, n := range elves {
-		if !n {
+		if n {
 			num++
 		}
 	}
 
 	return num
 }
-
