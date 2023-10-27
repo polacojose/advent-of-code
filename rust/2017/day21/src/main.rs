8b8ff5e20aa0b7a8d6d06a1b1a3fce3179206d19
Jose Polaco <polacojose@gmail.com>
2023-10-27 18:45:27 +0100

    Completed day21

--- rust/2017/day21/src/main.rs
+++ rust/2017/day21/src/main.rs
@@ -14,18 +14,23 @@ fn main() {
         .map(|x| x.parse().unwrap())
         .collect();
 
+    run(5, &rules);
+    run(18, &rules);
+}
+
+fn run(iterations: i32, rules: &Vec<Rule>) {
     let mut src_pattern = Pattern(vec![
         vec!['.', '#', '.'],
         vec!['.', '.', '#'],
         vec!['#', '#', '#'],
     ]);
 
-    println!("{}", &src_pattern);
-    for _ in 0..5 {
+    for _ in 0..iterations {
         src_pattern = transform_pattern(&rules, &src_pattern)
             .expect(format!("Failed to transform pattern:\n{}", src_pattern).as_str());
-        println!("{}", src_pattern)
     }
-
-    println!("On: {}", src_pattern.matches('#'));
+    println!(
+        "On After {iterations} iterations: {}",
+        src_pattern.matches('#')
+    );
 }
